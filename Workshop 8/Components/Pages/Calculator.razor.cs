14987a5bddf453b14e9f5f5a31a72a7718d52482
Prince <princeshrestha2111018@gmail.com>
2025-12-18 14:10:54 +0545

    second push

---
+++ Workshop 8/Components/Pages/Calculator.razor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Workshop_8.Components.Pages
+{
+    public partial class Calculator : ComponentBase
+    {
+        // Two-way binding variables
+        private double Number1 { get; set; }
+        private double Number2 { get; set; }
+
+        // Result
+        private double Result { get; set; }
+
+        // Event handling for buttons
+        private void Add() => Result = Number1 + Number2;
+        private void Subtract() => Result = Number1 - Number2;
+        private void Multiply() => Result = Number1 * Number2;
+        private void Divide()
+        {
+            if (Number2 != 0)
+                Result = Number1 / Number2;
+            else
+                Result = double.NaN;
+        }
+    }
+}
---
+++ Workshop 8/Components/Pages/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Workshop_8.Components.Pages
+{
+    internal class Product
+    {
+        public string Name { get; set; } = "";
+        public decimal Price { get; set; }
+        public int Likes { get; set; }
+        public string ImageUrl { get; set; } = "";
+    }
+}
